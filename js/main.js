f1b1d78a570fd61ea8cee1ac6b08296c78769530
Favour Godwin Arua <favourvalg@gmail.com>
2020-07-24 12:22:23 +0100

    third commit

--- js/main.js
+++ js/main.js
@@ -1,55 +1,31 @@
 
-function delay(n) {
-    n = n || 2000;
-    return new Promise(done => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
+// loader
+setTimeout(function(){
+    $('.loader-bg').fadeToggle();
+}, 1500);
+
+// top Button
+mybutton = document.getElementById("myBtn");
+window.onscroll = function(){
+    scrollFunction()
+};
+
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    }
+    else{
+        mybutton.style.display = "none";
+    }
 }
 
-function pageTransition() {
+function topFunction(){
+    document.body.scrollTop = 0;
 
-    var tl = gsap.timeline();
-
-    tl.to('.loading-screen', { duration: 1.2, width: '100%', left: '0', ease: 'Expo.easeInOut'});
-    tl.to('.loading-screen', { duration: 1, width: '100%', left: '100%', ease: 'Expo.easeInOut', delay: 0.3});
-    
-    tl.set('.loading-screen', { left: '-100%'});
-    
+    document.documentElement.scrollTop = 0;
 }
 
-funtion contentAnimation() {
-
-    var tl = gsap.timeline();
-    tl.from('.nav-link', { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2});
-
-}
-
-
-$(function() {
-    barba.init({
-
-    sync: true,
-
-    transitions: [{
-
-        async leave(data) {
-
-            const done = this.async();
-
-            pageTransition();
-            await delay(1000);
-            done();
-        },
-
-        async enter(data) {
-            contentAnimation();
-        },
-
-        async once(data) {
-            contentAnimation();
-        }
-    }]
-})
-})
+// AOS Animation
+AOS.init({
+    duration: 1200,
+  })
