02b1efab6e2cb3c2eab37b77560b3ac34f16ce45
Favour Godwin Arua <favourvalg@gmail.com>
2020-07-24 09:34:19 +0100

    third commit

--- js/main.js
+++ js/main.js
@@ -1,45 +1,55 @@
-// GSAP
-const anim = document.querySelector('li.nav-item');
 
-anim.addEventListener('click', animateFromTop);
-
-function animateFromTop(){
-    let tl = gsap.timeline();
-
-    tl.to('.overlay .layer', {
-        duration: 0.4,
-        height: '100%',
-        top: '0%',
-        delay: 0.2,
-        stagger: 0.05,
-    })
-
-    tl.to('.overlay .layer', {
-        duration: 0.4,
-        height: '100%',
-        top: '100%',
-        delay: 0.2,
-        stagger: -0.05,
-    })
-
-    tl.set('.overlay .layer', { top: '0', height: '0'});
+function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
 }
 
-    // top and bottom animation
+function pageTransition() {
 
-    // gsap.to(".layer1", {y: '100vh', delay: .5});
-    // gsap.to(".layer2", {y: '100vh', delay: .15});
-    // gsap.to(".layer3", {y: '100vh', delay: .25});
-    // gsap.to(".layer4", {y: '-100vh', delay: .5});
-    // gsap.to(".layer5", {y: '-100vh', delay: .15});
-    // gsap.to(".layer6", {y: '-100vh', delay: .25});
+    var tl = gsap.timeline();
 
-    // // left and right animation
+    tl.to('.loading-screen', { duration: 1.2, width: '100%', left: '0', ease: 'Expo.easeInOut'});
+    tl.to('.loading-screen', { duration: 1, width: '100%', left: '100%', ease: 'Expo.easeInOut', delay: 0.3});
+    
+    tl.set('.loading-screen', { left: '-100%'});
+    
+}
 
-    // gsap.to(".lay1", {y: '100vh', delay: .5});
-    // gsap.to(".lay2", {y: '100vh', delay: .15});
-    // gsap.to(".lay3", {y: '100vh', delay: .25});
-    // gsap.to(".lay4", {y: '-100vh', delay: .5});
-    // gsap.to(".lay5", {y: '-100vh', delay: .15});
-    // gsap.to(".lay6", {y: '-100vh', delay: .25});
+funtion contentAnimation() {
 
+    var tl = gsap.timeline();
+    tl.from('.nav-link', { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2});
+
+}
+
+
+$(function() {
+    barba.init({
+
+    sync: true,
+
+    transitions: [{
+
+        async leave(data) {
+
+            const done = this.async();
+
+            pageTransition();
+            await delay(1000);
+            done();
+        },
+
+        async enter(data) {
+            contentAnimation();
+        },
+
+        async once(data) {
+            contentAnimation();
+        }
+    }]
+})
+})
